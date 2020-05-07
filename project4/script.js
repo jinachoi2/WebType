98d684f2989d15f93181f120542b648e48f2bf12
Jina Choi <jchoi14@risd.edu>
2020-05-07 18:27:24 +0900

    progress
    final edits... need to organize css file and do final edits

--- project4/script.js
+++ project4/script.js
@@ -1,9 +1,61 @@
-// var tooltipSpan = document.getElementById('tooltip-span');
-//
-// window.onmousemove = function (e) {
-//     var x = e.clientX,
-//         y = e.clientY;
-//     tooltipSpan.style.top = (y + 20) + 'px';
-//     tooltipSpan.style.left = (x + 20) + 'px';
-//     console.log(x, y);
-// };
+$(document).ready(function(){
+  injectCalendar();
+});
+
+function injectCalendar(){
+  for(let i=0; i<library.length; i++){
+    $('#calendar').append(`
+      <div class="cal-grid img${i+1}">
+        <span
+          class="music-button"
+          onclick="play(this)"
+          data-title="${library[i].title}"
+          data-artist="${library[i].artist}"
+          data-mp3-url="${library[i].mp3}"
+          data-entrynumber="${library[i].entrynumber}"
+          data-entrylink="${library[i].entrylink}"
+        >
+          <img
+            src="${library[i].imgUrl}"
+            class="cal-img"
+          >
+          <span class="songname">${i+1}. ${library[i].title}</span>
+        </span>
+      </div>
+    `);
+  }
+}
+
+function play(el){
+  let mp3Url = $(el).attr('data-mp3-url');
+  console.log(mp3Url);
+  $('#player').attr('src', mp3Url);
+  console.log($('#player')[0]);
+  $('#player')[0].play();
+  $('.instruction').remove();
+  $('.player-container summary').empty();
+  $('.player-container summary').append(`
+    <h3>${ $(el).attr('data-title') }</h3>
+    <p>${ $(el).attr('data-artist') }</p>
+    <a href="${ $(el).attr('data-entrylink') }">
+      <p class="player-entry link">${ $(el).attr('data-entrynumber') }</p>
+    </a>
+  `);
+}
+
+    // <figure><img
+    //   src='${ $(el).children('img').attr('src') }'
+    // /></figure>
+
+
+// music player timeline:
+
+// https://stackoverflow.com/questions/39094273/html-audio-player-timeline-functionality-add/39095226
+// http://alexkatz.me/posts/building-a-custom-html5-audio-player-with-javascript/
+
+// audio.addEventListener("timeupdate", function(){
+//   var duration = this.duration;
+//   var currentTime = this.currentTime;
+//   var percentage = (currentTime / duration) * 100;
+//   playhead.style.left = percentage * 4 + 'px';
+// });
---
+++ project4/song-library.js
@@ -0,0 +1,98 @@
+let library = [
+  {
+    title: 'Busan',
+    artist: 'Damons Year',
+    mp3: 'songs/Busan.mp3',
+    imgUrl: 'images/entry1-album.jpeg',
+    entrynumber: 'Entry 1: Disturbance',
+    entrylink: 'entry1.html'
+  },
+  {
+    title: 'Dandelion',
+    artist: 'Oohyo',
+    mp3: 'songs/Dandelion.mp3',
+    imgUrl: 'images/entry2-album.jpg',
+    entrynumber: 'Entry 2: Canola Fields',
+    entrylink: 'entry2.html'
+  },
+  {
+    title: 'The End',
+    artist: 'Young B',
+    mp3: 'songs/TheEnd.mp3',
+    imgUrl: 'images/entry3-album.jpg',
+    entrynumber: 'Entry 3: Hyp.o.crite.s',
+    entrylink: 'entry3.html'
+  },
+  {
+    title: 'Howl&#39;s Moving Castle',
+    artist: 'Joe Hisaishi',
+    mp3: 'songs/Howl.mp3',
+    imgUrl: 'images/entry4-album.jpg',
+    entrynumber: 'Entry 4: Piano Man',
+    entrylink: 'entry4.html'
+  },
+  {
+    title: 'Modern Loneliness',
+    artist: 'Lauv',
+    mp3: 'songs/ModernLoneliness.mp3',
+    imgUrl: 'images/entry5-album.jpg',
+    entrynumber: 'Entry 5: Alone Together',
+    entrylink: 'entry5.html'
+  },
+  {
+    title: 'A Thousand Winds',
+    artist: 'Lim Hyung Joo',
+    mp3: 'songs/aThousandWinds.mp3',
+    imgUrl: 'images/entry6-album.jpg',
+    entrynumber: 'Entry 6: Tragedy',
+    entrylink: 'entry6.html'
+  },
+  {
+    title: 'Spirits of the Sea',
+    artist: 'Kenshi Yonezu',
+    mp3: 'songs/spiritsOfTheSea.mp3',
+    imgUrl: 'images/entry7-album.jpg',
+    entrynumber: 'Entry 7: Wishes',
+    entrylink: 'entry7.html'
+  },
+  {
+    title: 'lovememore',
+    artist: 'dosii',
+    mp3: 'songs/lovememore.mp3',
+    imgUrl: 'images/entry8-album.jpg',
+    entrynumber: 'Entry 8: City Pop',
+    entrylink: 'entry8.html'
+  },
+  {
+    title: 'Palette',
+    artist: 'IU',
+    mp3: 'songs/palette.mp3',
+    imgUrl: 'images/entry9-album.jpg',
+    entrynumber: 'Entry 9: Coincidence',
+    entrylink: 'entry9.html'
+  },
+  {
+    title: 'Fly High!!',
+    artist: 'BURNOUT SYNDROMES',
+    mp3: 'songs/flyhigh.mp3',
+    imgUrl: 'images/entry10-album.jpg',
+    entrynumber: 'Entry 10: Volleyball 1',
+    entrylink: 'entry10.html'
+  },
+  {
+    title: 'Black Swan',
+    artist: 'BTS',
+    mp3: 'songs/blackSwan.mp3',
+    imgUrl: 'images/entry11-album.jpg',
+    entrynumber: 'Entry 11: Volleyball 2',
+    entrylink: 'entry11.html'
+  },
+  {
+    title: 'always, i&#39;ll care',
+    artist: 'Jeremy Zucker',
+    mp3: 'songs/zucker.mp3',
+    imgUrl: 'images/entry12-album.jpeg',
+    entrynumber: 'Entry 12: Peace',
+    entrylink: 'entry12.html'
+  }
+];
